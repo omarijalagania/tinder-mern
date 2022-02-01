d37b89884b653fd7229191d2d1f06e817b447420
omari <omari.jalagania@gmail.com>
2022-02-02 01:35:46 +0400

    done with styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import Header from "./components/Header";
 import TinderCards from "./components/TinderCards";
-
+import SwipeButtons from "./components/SwipeButtons";
 function App() {
   return (
     <div className="App">
       <Header />
       <TinderCards />
+      <SwipeButtons />
     </div>
   );
 }
